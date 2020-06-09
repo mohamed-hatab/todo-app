0e85f80d910d154ca55d08d64f77c992be9da36c
unknown <mohamedkratos40@gmail.com>
2020-06-09 17:20:43 +0200

    fixed issue

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const mongoose = require("mongoose")
 const userRouter = require(__dirname + "/router/user.js")
 const taskRouter = require(__dirname + "/router/task.js")
 
-mongoose.connect(process.env.DB_STRING || 'mongodb://localhost:27017/database', {
+mongoose.connect(process.env.DB_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-app.listen("3000", () => console.log("server is running"))
+app.listen(process.env.PORT, () => console.log("server is running"))
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+app.git("/",()=>{res.send("hi")})
 
 console.log(process.env.API_KEY);
